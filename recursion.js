739bd9662e82c91fa1c75a3119af0bc17ddc774b
Roarka <designerexpert@gmail.com>
2017-11-08 14:14:32 -0500

    Initial Commit

--- recursion.js
+++ recursion.js
@@ -1,15 +1,21 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
 
 // write a recursive - function called countToTen that mimics the while loop above.
-
 // code here
+const countToTen = () => {
+  for (let i = 0; i <= 10; i++) {
+    console.log('countToTen Loop', i);
+  }
+};
+
+countToTen();
 
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
